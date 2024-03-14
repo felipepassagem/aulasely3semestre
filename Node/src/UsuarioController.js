9eae3c1f200cc1c1c8fac747c4589b068ec5314e
Felipe <felipe.passagem@gmail.com>
2024-03-14 13:26:48 -0300

    .

--- Node/src/UsuarioController.js
+++ Node/src/UsuarioController.js
@@ -6,7 +6,7 @@ async function connect() {
         const connection = await mysql.createConnection(
             {host:'54.91.193.137',
                   user: 'libertas',
-                  passqord: '123456',
+                  password: '123456',
                   database: 'libertas5per'
         });
         console.log("Conectou");
@@ -14,25 +14,68 @@ async function connect() {
         return connection;
 }
 
-exports.post = (req, res, next) => {
-    res.status(201).send('Rota POST')
+exports.post = async (req, res, next) => {
+    const conn = await connect();
+    const sql = "INSERT INTO usuario (nome, telefone, email, senha) VALUES (?,?,?,?)";
+
+    const values = [req.body.nome, req.body.telefone, req.body.email, req.body.senha];
+
+    // for (var i = 0; i < 10000; i++) {
+    //     console.log(i)
+       
+    // }
+    await conn.query(sql, values)
+
+    res.status(201).send('Rota POST');
 };
 
-exports.put = (req, res, next) => {
-    let id = req.params.id;
-    res.status(201).send(`Rota PUT com ID! ${id}`);
+exports.put = async (req, res, next) => {
+    const conn = await connect();
+    const sql = "UPDATE usuario SET nome = ?, telefone= ?, email = ?, senha = ? where idusuario = ?";
+
+    const values = [req.body.nome, req.body.telefone, req.body.email, req.body.senha, req.params.id];
+    
+    await conn.query(sql, values)
+
+    res.status(201).send("ROTA PUT");
 }
 
-exports.delete = (req, res, next) => {
-    let id = req.params.id;
-    res.status(200).send("Rota DELETE com ID! " + id );
+exports.delete = async (req, res, next) => {
+    const conn = await connect();
+    const sql = "DELETE FROM usuario WHERE idusuario = ?";
+
+    const values = [req.params.id];
+    
+    await conn.query(sql, values)
+
+    res.status(201).send("ROTA DELETE");
 }
 
-exports.get = (req, res, next) => {
-    res.status(200).send('Rota GET!');
+exports.get = async (req, res, next) => {
+    const conn = await connect();
+    const sql = "SELECT * FROM usuario";
+
+    const values = [req.params.id];
+    
+    var rows = await conn.query(sql, values)
+
+    res.status(201).send(rows);
 }
 
-exports.getById = (req, res, next) => {
-    let id = req.params.id;
-    res.status(200).send(`Rota GET com ID! ${id}`);
+
+exports.getById = async (req, res, next) => {
+    const conn = await connect();
+    const sql = "SELECT * FROM usuario WHERE idusuario = ?";
+
+    const values = [req.params.id];
+    
+    var [rows] = await conn.query(sql, values)
+
+    if (rows.length > 0) {
+        res.status(201).send(rows);
+    } else {
+        res.status(404).send("Usuario não encontrado")
+    }
+
+    
 }
