9daffe93f11ba1d08d893435c793b81bca0b0dd7
Felipe <felipe.passagem@gmail.com>
2024-04-02 19:44:04 -0300

    .

--- Node/public/index.js
+++ Node/public/index.js
@@ -41,16 +41,18 @@ function listar(){
 function mostrar(dados){
     // dados = dados[0]
     const lista = document.getElementById("table");
+    
 
     lista.innerHTML = "";
 
     for(var i in dados) {
+        let id = dados[i].idusuario;
         lista.innerHTML += "<tr>"
-        + "<td>" + dados[i].idusuario + "</td>"
+        + "<td>" + id + "</td>"
         + "<td>" + dados[i].nome + "</td>" 
         + "<td>" + dados[i].telefone + "</td>" 
         + "<td>" + dados[i].email + "</td>" 
-        + "<td> <button class='btn btn-success'><i class='bi bi-pencil'></i></button> <button class='btn btn-danger'><i class='bi bi-trash'></i></button></td>"
+        + "<td> <button class='btn btn-success' onclick=alterar("+id+")><i class='bi bi-pencil'></i></button> <button class='btn btn-danger' onclick=exccluir("+id+")><i class='bi bi-trash'></i></button></td>"
         + "</tr>"
     }
 }
@@ -63,7 +65,50 @@ function excluirSim(){
     
 }
 
-function salvar(){
+// Função fetch com timeout
+function fetchWithTimeout(url, options, timeout = 5000) {
+    return Promise.race([
+        fetch(url, options),
+        new Promise((_, reject) =>
+            setTimeout(() => reject(new Error('Timeout')), 5000)
+        )
+    ]);
+}
+
+// function salvar(){
+//     const txtnome = document.getElementById("txtnome");
+//     const txttelefone = document.getElementById("txttelefone");
+//     const txtemail = document.getElementById("txtemail");
+//     const txtsenha = document.getElementById("txtsenha");
+
+//     const dados = {
+//         nome: txtnome.value,
+//         telefone: txttelefone.value,
+//         email: txtemail.value,
+//         senha: txtsenha.value
+//     }
+    
+//     if (idatual <= 0) {
+//         //inserir
+//         fetch("http://127.0.0.1:3333/usuario/",
+//             {
+//                 headers: {
+//                     'Accept': 'application/json',
+//                     'Content-Type': 'application/json'
+
+//                 },
+                
+//                 method: "POST", body: JSON.stringify(dados)}
+//                 ).then(() => {
+//                     modal.hide();
+//                     listar();
+//                 })
+//     } else {
+//         //alterar
+//     }
+// }
+
+function salvar() {
     const txtnome = document.getElementById("txtnome");
     const txttelefone = document.getElementById("txttelefone");
     const txtemail = document.getElementById("txtemail");
@@ -75,25 +120,33 @@ function salvar(){
         email: txtemail.value,
         senha: txtsenha.value
     }
-    console.log(JSON.stringify(dados))
+
     if (idatual <= 0) {
-        //inserir
-        fetch("http://127.0.0.1:3333/usuario/",
+        // Inserir
+        fetchWithTimeout("http://127.0.0.1:3333/usuario/",
             {
                 headers: {
-                    'Accept': 'aplication/json',
+                    'Accept': 'application/json',
                     'Content-Type': 'application/json'
-
                 },
-                
-                method: "POST", body: JSON.stringify(dados)}
-                ).then(() => {
-                    modal.hide();
-                    listar();
-                })
+                method: "POST",
+                body: JSON.stringify(dados)
+            },
+            0 // Defina o tempo limite em milissegundos aqui
+        )
+        .then(() => {
+            console.log("ASDASDASDASD")
+            modal.hide();
+            listar();
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+            // Lidar com erros, incluindo timeout
+        });
     } else {
-        //alterar
+        // Alterar
     }
+    
 }
 
 listar()
